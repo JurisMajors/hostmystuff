14166bcf41a0e30971d4f12143ba067d0a83ea07
Juris Majors <jurismajors@gmail.com>
2019-09-26 00:58:09 +0200

    Convert days to ms for clearing age

--- src/clearer.js
+++ src/clearer.js
@@ -8,7 +8,7 @@ const MAX_SIZE_IN_MIB = 512;
 const getClearingAge = (minAge, maxAge, sizeInBytes) => {
     const sizeInMiB = sizeInBytes * BYTES_IN_MIB;
     // see 0x0.st for this formula
-    return minAge + (-maxAge + minAge) * Math.pow((sizeInMiB / MAX_SIZE_IN_MIB - 1), 3)
+    return (minAge + (-maxAge + minAge) * Math.pow((sizeInMiB / MAX_SIZE_IN_MIB - 1), 3)) * 86400000
 }
 
 function clearOldFiles(FILE_DIR, CLEARING_MIN_AGE, CLEARING_MAX_AGE) {
