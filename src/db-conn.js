7337cd97855e311b1f0ca59e5c75f6a5e5181fdc
JurisMajors <j.a.majors@student.tue.nl>
2019-10-04 19:25:56 +0200

    Implementations for basic key-management functions

--- src/db-conn.js
+++ src/db-conn.js
@@ -12,28 +12,76 @@ function establishConnection(callback) {
     );
 }
 
-function initialzeDB() {
-    establishConnection((db) => {
-        console.log("Connected to database");
-        const dbo = db.db("keys");
-        dbo.createCollection("userKeys", function(err) {
-            if (err) throw err;
-            console.log("Initialized the userKeys collection");
-        });
-        db.close();
-    })
+function initializeDB() {
+    establishConnection(
+        (db) => {
+            console.log("Connected to database");
+            const dbo = db.db("keys");
+            dbo.createCollection("userKeys", 
+                (err) => {
+                    if (err) throw err;
+                    console.log("Initialized the userKeys collection");
+                }
+            );
+            db.close();
+        }
+    );
 }
+
 
 function existsAPIKey(uuid) {
-    return true;
+    if (getKeyFromDB(uuid)) {
+        return true;
+    }
+    return false;
 }
 
-function canUpload(apiKey, fileSize) {
-    return true;
+function getKeyFromDB(apiKey) {
+    let keyInfo;
+
+    establishConnection(
+        (db) => {
+            const keyCollection = db.db("keys").collection("userKeys");
+            keyCollection.find({_id : uuid}).toArray(
+                (err, items) => {
+                    console.log(keyInfo);
+                    keyInfo = items[0];
+                }
+            );
+        }
+    );
+    return keyInfo;
 }
 
-function addFileToKey(apiKey, fileName) {
+// assumes existsAPIKey(apiKey) == true
+function enoughCapacity(apiKey, fileSize) {
+    const keyInfo = getKeyFromDB(apiKey);
+    return keyInfo.capacityLeft - fileSize >= 0;
+}
 
+function addFileToKey(apiKey, fileName, fileSize) {
+    const keyInfo = getKeyFromDB(apiKey);
+    const curFiles = keyInfo.files;
+    const newCapacity = keyInfo.capacityLeft - fileSize;
+    curFiles.push(fileName);
+
+    establishConnection(
+        (db) => {
+            const keyCollection = db.db("keys").collection("userKeys");
+            keyCollection.update(
+                { _id : apiKey },
+                { $set : 
+                    {
+                        files : curFiles,
+                        capacityLeft : newCapacity 
+                    }
+                },
+                (err, res) => {
+                    if (err) throw err
+                }
+            )
+        }
+    );
 }
 
 function deleteFile(apiKey, fileName) {
@@ -41,6 +89,6 @@ function deleteFile(apiKey, fileName) {
 }
 
 module.exports = {
-    initialConn : initialzeDB,
+    initialConn : initializeDB,
     dbConn : establishConnection
 }
