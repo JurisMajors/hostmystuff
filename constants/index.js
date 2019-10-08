fdc4b616858858170b22160d8515c5ce8c5e0fc6
Juris Majors <jurismajors@gmail.com>
2019-10-08 10:45:27 +0200

    Fix path to file directory

--- constants/index.js
+++ constants/index.js
@@ -5,7 +5,7 @@ module.exports = Object.freeze({
   CLEARING_MAX_AGE: DAY_IN_MS * 7, // files stay up to a week
   CLEARING_MIN_AGE: DAY_IN_MS, // files stay at least a day
   CLEARING_FREQUENCY: DAY_IN_MS / 3,
-  FILE_DIR: path.join(__dirname, '/files/'),
+  FILE_DIR: path.join(path.resolve(__dirname,'..'), '/files/'),
   ADDRESS: '127.0.0.1', // server address,
   MONGO_URI_LOCAL: 'mongodb://127.0.0.1:27017/keys', // DB connection string
   BYTES_IN_MIB: 1048576,
